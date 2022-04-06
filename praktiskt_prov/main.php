a1fce101ed7a26b949229bb6af00eb4f19773c9e
l4mers <94372897+l4mers@users.noreply.github.com>
2022-04-06 23:35:49 +0200

    Kallabah

--- praktiskt_prov/main.php
+++ praktiskt_prov/main.php
@@ -1,18 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movie Library</title>
-</head>
-<body>
-    <?php
+<?php
+require_once 'logindata.php';
+try{$pdo = new PDO($attr, $user, $pass, $opts);} //Ett försök att skapa ett PDO gränssnitt med variabelvärden från logindata.php där värderna sparas
+    //i våran variabel pdo.
+    //Om inta databasen nås så skapar vi en fel hantering som ger oss en felmedelningsvärde samt meddelar användaren att systemet är nere(Detta är bortom användarens kapacitet att påverka)
+catch(PDOExeption $e){throw new PDOException($e->getMessage(), (int)$e->getCode());
 
-    
+$query = "SELECT * FROM genre";
+$genreres = $pdo->query($query);
+
+$row = $genreres->fetch();
+$genre_id = $row['genre_id'];
+$genre = $row['genre'];
 
 
 
-    ?>
-</body>
-</html>
+function Manage_String($pdo, $string)
+{
+    $string = $pdo->quote($string);
+    $string = Fix_String($string);
+    return $string;
+}
+function Fix_String($string)
+{
+    if (get_magic_quotes_gpc())
+    {
+        $string = stripslashes($string);
+    }
+    $string = strip_tags($string);
+    $string = htmlentities($string);
+    return $string;
+}
+?>
